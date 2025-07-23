9a4202c8fff00259d3f261e1c2872cf2cd4ca6a6
Sam.Deverett <Sam.Deverett@dsit.gov.uk>
2025-07-23 17:00:58 +0000

    Comment out solution variant.

--- task.py
+++ task.py
@@ -11,13 +11,12 @@ from inspect_cyber import create_agentic_eval_dataset
 def doomla():
     return Task(
         dataset=(
-            create_agentic_eval_dataset(
-                root_dir=Path("evals/doomla").resolve()
-            ).filter_by_metadata({"variant_name": "solution"})
+            create_agentic_eval_dataset(root_dir=Path("evals/doomla").resolve())
+            # .filter_by_metadata({"variant_name": "solution"})
         ),
         solver=react(tools=[bash(), python()]),
         scorer=includes(),
     )
 
 
-eval(doomla, model="anthropic/claude-sonnet-4-20250514", token_limit=10_000)
+eval(doomla, model="anthropic/claude-sonnet-4-20250514", token_limit=5_000)
